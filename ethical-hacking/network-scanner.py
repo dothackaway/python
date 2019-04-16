0bbda5583da80706e840c79abcc1e27227fc357d
dothackaway <dothackaway@gmail.com>
2019-04-16 17:36:05 -0400

    Added variable to create and store ARP packet

--- ethical-hacking/network-scanner.py
+++ ethical-hacking/network-scanner.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+#
+# Tool for discovering clients on the same network
+# 1. Create arp request packet directed to broadcast MAC asking for IP
+# 2. Send packet and receive response
+# 3. Parse the response
+# 4. Print result
+#
+
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    # Create ARP packet using scapy
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+
 
 scan("192.168.43.230")
